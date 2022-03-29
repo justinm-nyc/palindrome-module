6b768d2679415efebac959ddcac61ae68a315441
justinm-nyc <justin.martinelli@homechef.com>
2022-03-29 11:23:28 -0400

    New module

--- lib/jmart_video_palindrome.rb
+++ lib/jmart_video_palindrome.rb
@@ -1,6 +1,23 @@
 require "jmart_video_palindrome/version"
 
 module JmartVideoPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+  
+  # Returns content for palindrome testing
+  def processed_content
+      self.to_s.scan(/[a-z\d]/i).join.downcase
+  end
+end
+
+class String
+  include JmartVideoPalindrome
+end
+
+class Integer
+  include JmartVideoPalindrome
 end
--- test/jmart_video_palindrome_test.rb
+++ test/jmart_video_palindrome_test.rb
@@ -1,11 +1,27 @@
 require "test_helper"
 
 class JmartVideoPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::JmartVideoPalindrome::VERSION
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome? 
+  end
+
+  def test_non_palindrome_with_punctuation
+    assert "Madam, I'm Adam".palindrome?
+  end
+
+  def test_integer_non_palindrome
+    refute 12345.palindrome?
+  end
+
+  def test_integer_palindrome
+    assert 12321.palindrome?
   end
 end
